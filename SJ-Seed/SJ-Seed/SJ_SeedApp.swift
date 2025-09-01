bff15ea53e027fde387949fee852878603bafb47
nayoung.kim <jenny6816@naver.com>
2025-09-01 22:56:23 +0900

    [Feat] 새 프로젝트 파일 생성

---
+++ SJ-Seed/SJ-Seed/SJ_SeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SJ_SeedApp.swift
+//  SJ-Seed
+//
+//  Created by 김나영 on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SJ_SeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
