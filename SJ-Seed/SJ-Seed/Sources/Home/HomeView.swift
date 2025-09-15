c57d5272ead3cdaa5d7b4a30d7fd254b01a744a2
nayoung.kim <jenny6816@naver.com>
2025-09-16 00:14:45 +0900

    [Feat] 홈화면 버튼 컴포넌트 구현

---
+++ SJ-Seed/SJ-Seed/Sources/Home/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  SJ-Seed
+//
+//  Created by 김나영 on 9/15/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            VStack {
+                Spacer()
+                HStack {
+                    MainButtonComponent(buttonImage: Image(.student), buttonText: "도감")
+                    MainButtonComponent(buttonImage: Image(.student), buttonText: "식물")
+                    MainButtonComponent(buttonImage: Image(.student), buttonText: "병원")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ SJ-Seed/SJ-Seed/Sources/Home/MainButtonComponent.swift
@@ -0,0 +1,28 @@
+//
+//  MainButtonComponent.swift
+//  SJ-Seed
+//
+//  Created by 김나영 on 9/15/25.
+//
+
+import SwiftUI
+
+struct MainButtonComponent: View {
+    var buttonImage: Image
+    var buttonText: String
+    
+    var body: some View {
+        VStack {
+            buttonImage
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 170)
+                .clipped()
+            Text(buttonText)
+        }
+    }
+}
+
+#Preview {
+    MainButtonComponent(buttonImage: Image(.student), buttonText: "도감")
+}
