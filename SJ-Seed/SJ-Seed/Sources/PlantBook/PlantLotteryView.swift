57797a6fa65f71f714f79d8d40adffa2d4eb2e05
nayoung.kim <jenny6816@naver.com>
2025-10-29 23:20:19 +0900

    [Feat] 뽑기 버튼 누를 시 gif 재생

--- SJ-Seed/SJ-Seed/Sources/PlantBook/PlantLotteryView.swift
+++ SJ-Seed/SJ-Seed/Sources/PlantBook/PlantLotteryView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct PlantLotteryView: View {
-    @State private var showFirst = true
+    @State private var showFirst = false
+    @State private var isAnimating = false   // 애니메이션 시작 여부
+    @State private var showText = false      // "두근두근..." 텍스트 표시
     let timer = Timer.publish(every: 0.4, on: .main, in: .common).autoconnect()
     
     var body: some View {
@@ -18,19 +20,34 @@ struct PlantLotteryView: View {
                 .scaledToFill()
                 .ignoresSafeArea()
             
-//            VStack {
-                Image(showFirst ? "gacha1" : "gacha2")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 350)
-                    .onReceive(timer) { _ in
-                        withAnimation(.easeInOut(duration: 0.3)) {
-                            showFirst.toggle()
+            Image(showFirst ? "gacha1" : "gacha2")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 350)
+                .offset(y: -50)
+                .onReceive(timer) { _ in
+                    guard isAnimating else { return }
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        showFirst.toggle()
+                    }
+                }
+            
+            if !isAnimating {
+                Button(action: {
+                    withAnimation(.easeInOut) {
+                        isAnimating = true
+                        showText = true
+                    }
+                    
+                    // 3초 뒤 애니메이션 종료
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        withAnimation {
+                            isAnimating = false
+                            showText = false
+                            // TODO: 다음 단계 (ex. 결과 뽑기 화면으로 전환)
                         }
                     }
-                    .offset(x: 0, y: -50)
-                
-                Button(action: { }) {
+                }) {
                     Text("1000코인 지불 후 뽑기")
                         .foregroundStyle(.ivory1)
                         .font(Font.OwnglyphMeetme.regular.font(size: 22))
@@ -40,8 +57,14 @@ struct PlantLotteryView: View {
                                 .frame(width: 220, height: 50)
                         )
                 }
-                .offset(x: 0, y: 150)
-//            }
+                .offset(y: 150)
+            } else if showText {
+                Text("두근두근...")
+                    .foregroundStyle(.brown1)
+                    .font(Font.OwnglyphMeetme.regular.font(size: 28))
+                    .offset(y: 150)
+                    .transition(.opacity.combined(with: .scale))
+            }
         }
     }
 }
