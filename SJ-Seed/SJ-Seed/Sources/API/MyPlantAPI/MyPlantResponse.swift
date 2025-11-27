80af79638a1699cf6bd2e078eb9982803ac9f544
nayoung.kim <jenny6816@naver.com>
2025-11-28 03:44:07 +0900

    [Bug] 식물 등록 안되는 버그 수정

--- SJ-Seed/SJ-Seed/Sources/API/MyPlantAPI/MyPlantResponse.swift
+++ SJ-Seed/SJ-Seed/Sources/API/MyPlantAPI/MyPlantResponse.swift
@@ -9,11 +9,11 @@ import Foundation
 
 // MARK: - 식물 등록 (POST /member/plant/{memberId})
 struct RegisterPlantResult: Codable {
-    let id: Int
+    let id: Int?
     let name: String
     let broughtDate: String
     let memberId: Int
-    let speciesId: Int
+//    let speciesId: Int
 }
 
 // MARK: - 식물 리스트 조회 (GET /member/plantList/{memberId})
